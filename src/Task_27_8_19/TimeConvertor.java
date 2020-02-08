ab9122e84e8afc708194cae96cb643347498c295
Amrendra Bagga <amrendrabagga@gmail.com>
2020-02-08 17:54:00 +0530

    commiting via staging in feature-mam4

--- src/Task_27_8_19/TimeConvertor.java
+++ src/Task_27_8_19/TimeConvertor.java
@@ -18,6 +18,7 @@ public class TimeConvertor {
 
 		System.out.println("hello from git terminal-3 for stash");
 		System.out.println("hello from feature-mam-3 for stash");
+		System.out.println("hello from master on local");
 	}
 
 }
