0b99513a82de34e4348e586900fd1916b62ef250
Amrendra Bagga <amrendrabagga@gmail.com>
2020-02-08 12:53:39 +0530

    commiting time convertor files to branch

---
+++ src/Task_27_8_19/TimeConvertor.java
@@ -0,0 +1,15 @@
+package Task_27_8_19;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeConvertor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LocalDateTime ldt = LocalDateTime.parse("1997-10-25 10:10", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+		System.out.println(ldt);
+	}
+
+}
