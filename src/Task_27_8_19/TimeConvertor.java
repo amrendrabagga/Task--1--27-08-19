5af4d5738328761709c82ed3f428e1fabf5de8fd
Amrendra Bagga <amrendrabagga@gmail.com>
2020-02-08 14:55:10 +0530

    commiting sout from feature-mam-3 for stash

--- src/Task_27_8_19/TimeConvertor.java
+++ src/Task_27_8_19/TimeConvertor.java
@@ -15,6 +15,9 @@ public class TimeConvertor {
 		System.out.println("hello from git terminal-2");
 		
 		System.out.println("hello from feature-mam-2");
+
+		System.out.println("hello from git terminal-3 for stash");
+		System.out.println("hello from feature-mam-3 for stash");
 	}
 
 }
